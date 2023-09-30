fcf4ead5b255c46741f664e063ff415d2c30deed
manishasharma2 <manishasharma192003@gamil.com>
2023-09-30 22:07:05 +0530

    tct

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TicTacToe from './Components/TikTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div >
+      
+    <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
